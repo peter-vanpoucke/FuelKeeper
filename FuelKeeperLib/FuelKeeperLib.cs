210905240fea3891b5c322a4ea0eee298dfe170e
Peter Vanpoucke <peter.vanpoucke@subport.be>
2016-06-19 14:49:37 +0200

    Eerste opbouw

---
+++ FuelKeeperLib/FuelKeeperLib.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace be.subport.app.FuelKeeper.Lib
+{
+    public class FuelKeeperLib : ContentPage
+    {
+        public FuelKeeperLib()
+        {
+            var button = new Button
+            {
+                Text = "Click Me!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            int clicked = 0;
+            button.Clicked += (s, e) => button.Text = "Clicked: " + clicked++;
+
+            Content = button;
+        }
+    }
+}
---
+++ FuelKeeperSrvLib/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be.subport.app.FuelKeeper.SrvLib
+{
+    public static class Helper
+    {
+        public static DirectoryInfo GetMainDir()
+        {
+            var myType = typeof(Helper);
+            var program = new FileInfo(myType.Assembly.Location);
+            var programDir = program.Directory;
+
+            DirectoryInfo diMaindir = null;
+
+            var searchDir = programDir;
+            while (searchDir != null)
+            {
+                if (searchDir.EnumerateFiles("maindir.indicator").Count() == 1)
+                {
+                    diMaindir = searchDir;
+                    break;
+                }
+                searchDir = searchDir.Parent;
+            }
+
+            return diMaindir;
+        }
+    }
+}
